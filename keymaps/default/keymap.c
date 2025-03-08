bec2e93c98a074ad14d8ac7e2d52e1ddd55a9b3c
Frank-Pepper <reppepf@gmail.com>
2025-03-08 20:08:08 +0100

    mouse works, combo added

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,6 +3,22 @@
 
 #include QMK_KEYBOARD_H
 
+#define MOUSE TG(_MOUSE)
+
+enum custom_layers {
+    _BASE,
+    _NUMS,
+    _MOUSE,
+    _FUNC,
+  };
+
+const uint16_t PROGMEM test_combo1[] = {KC_A, KC_B, COMBO_END};
+const uint16_t PROGMEM toggle_nav_combo[] = {KC_J, KC_K, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(test_combo1, KC_ESC),
+    COMBO(toggle_nav_combo, MOUSE), // keycodes with modifiers are possible too!
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
@@ -18,32 +34,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
-                                            KC_BSPC, KC_LGUI, MO(1),           KC_SPC,  KC_ENT,  KC_RALT
+    [_BASE] = LAYOUT_split_3x6_3(
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|  
+                                            KC_BSPC, KC_LGUI, MO(1),       KC_SPC, KC_ENT,  KC_RALT
+                                        //`--------------------------'  `--------------------------'
+
     ),
-    [1] = LAYOUT_split_3x6_3( \
-      //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-          _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_VOLU ,
-      //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-          _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLD ,
-      //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-          QK_BOOT, XXXXXXX , KC_TILD,KC_GRV, KC_LBRC, KC_LCBR,                      KC_RCBR, KC_RBRC, KC_COMM,KC_DOT,  KC_SLSH, _______ ,
-      //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                              KC_TRNS,  KC_TRNS, KC_TRNS,    TG(2), KC_TRNS, KC_COLON
-                                          //`--------------------------'  `--------------------------'
+    [_NUMS] = LAYOUT_split_3x6_3( \
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_VOLU,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLD,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        QK_BOOT, XXXXXXX, KC_TILD, KC_GRV,  KC_LBRC, KC_LCBR,                      KC_RCBR, KC_RBRC, KC_COMM, KC_DOT,  KC_SLSH, _______,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            KC_TRNS, KC_TRNS, KC_TRNS,    MOUSE,   KC_LGUI, KC_COLON
+                                        //`--------------------------'  `--------------------------'
       ),
-      [2] = LAYOUT_split_3x6_3( \
-        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-            _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_VOLU ,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            _______, KC_1,    KC_2,   KC_3,    KC_7,    KC_6,                         KC_9,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLD ,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            QK_BOOT, XXXXXXX , KC_TILD,KC_GRV, KC_LBRC, KC_LCBR,                      KC_RCBR, KC_RBRC, KC_COMM,KC_DOT,  KC_SLSH, _______ ,
-        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                KC_TRNS,  KC_TRNS, KC_TRNS,    TG(2), KC_TRNS, KC_COLON
-                                            //`--------------------------'  `--------------------------'
+      [_MOUSE] = LAYOUT_split_3x6_3( \
+    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, MS_BTN1, MS_WHLU, MS_BTN2, KC_RPRN, KC_VOLU,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, XXXXXXX,                      XXXXXXX, MS_LEFT, MS_UP,   MS_DOWN, MS_RGHT, KC_VOLD,
+    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        QK_BOOT, XXXXXXX, KC_TILD, KC_GRV, KC_LBRC, KC_LCBR,                       KC_RCBR, MS_WHLL, MS_WHLD, MS_WHLR, KC_SLSH, _______,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            KC_TRNS,  KC_TAB, MOUSE,      MS_BTN1, MS_BTN2, MS_BTN3
+                                        //`--------------------------'  `--------------------------'
         ),
 };
